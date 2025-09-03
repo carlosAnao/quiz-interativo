3d9d2309e1ef9e11b8954cb7e3b15e7118cfb19f
carlosAnao <joao.zibarth.santos@escola.pr.gov.br>
2025-09-03 07:42:34 -0300

    v

--- js/script.js
+++ js/script.js
@@ -3,18 +3,45 @@ import {perguntas} from './perguntas.js';
 
 const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
-const caixaAlternativaas = document.querySelector(".caixa-alternativa");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const botaoIniciar = document.querySelector(".iniciar-btn");
 const telaInicial = document.querySelector(".tela-inicial");
 
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
 
 botaoIniciar.addEventListener('click', iniciaJogo);
 
 function iniciaJogo() {
-    atual = 0;
-    telaInicial.style.display = 'none'
-    caixaPerguntas.classList.remove("")
+  atual = 0;
+  telaInicial.style.display = 'none';
+  caixaPerguntas.classList.remove(".mostrar");
+  caixaAlternativas.classList.remove(".mostrar");
+  caixaResultado.classList.remove(".mostrar");
+  mostraPergunta();
 }
+
+function mostraPergunta() {
+    if(atual >= perguntas.length){
+        mostraResultado();
+        return;
+    }
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
+    mostraAlternativas();
+}
+
+function mostraAlternativas() {}
+
+function mostraResultado() {
+    caixaPerguntas.textContent = `Após tudo isso, ${nome} descobriu que`;
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+    caixaResultado.classList.add(".mostrar");
+    botaoJogarNovamente.addEventListener("click", jogarNovamente);
+}
+
+function jogarNovamente() {}
