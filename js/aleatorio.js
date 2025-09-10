bbfa25fd839c3c71ee5505c843fbfed17e435709
carlosAnao <joao.zibarth.santos@escola.pr.gov.br>
2025-09-10 07:37:12 -0300

    2

---
+++ js/aleatorio.js
@@ -0,0 +1,8 @@
+const nomes = ["Juca", "Emerson", "Osvaldo", "Pipokinha", "Deolane", "Virginia"];
+
+function aleatorio(lista){
+  const posicao = Math.floor(Math.random()* lista.length);
+  return lista[posicao];
+}
+
+export const nome = aleatorio(nomes) 
